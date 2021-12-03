34a5821e4ea289817b9f043a6ff172aeea371b4e
keleslz <mha.elhabib@gmail.com>
2021-12-03 20:44:30 +0100

    init project

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="mb-12">Website</h2>
+      <div >My website</div>
+    </div>
+  );
+}
+
+export default App;
